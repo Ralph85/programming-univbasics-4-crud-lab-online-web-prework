52e1a3760585627f344e71037b7e9c51b233bdcd
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-23 21:36:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,8 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  heroes = ["Green Arrow", "Flash", "Supergirl", "White Canary"]
-  heroes.shift("wow")
+  array = ["wow", "I", "am", "really", "learning"]
+  arrayshift("wow")
 end
 
 def retrieve_element_from_index(array, index_number)
