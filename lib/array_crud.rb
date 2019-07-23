9d6384b19baf24ab6fabf84028dbaecf231fbb5e
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-23 21:04:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   heroes = ["Green Arrow", "Flash", "Supergirl", "White Canary"]
-  heroes.unshift("wow!")
+  heroes.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
