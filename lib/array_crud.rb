8fb38d81262f9f9bf4143204b6358362f8e87888
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-23 20:42:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  num = []
+  heroes = []
 end
 
 def create_an_array
-  num = [1, 2, 3]
+  heroes = ["Green Arrow", "Flash", "Supergirl", "White Canary"]
 end
 
 def add_element_to_end_of_array(array, element)
