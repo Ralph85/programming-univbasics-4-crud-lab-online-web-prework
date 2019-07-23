a5caf05bc9fa34273f881809f39c00230511cfdc
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-23 21:14:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   heroes = ["Green Arrow", "Flash", "Supergirl", "White Canary"]
-  heroes.pop("arrays!")
+  heroes.pop("White Canary")
 end
 
 def remove_element_from_start_of_array(array)
